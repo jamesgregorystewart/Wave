bcd0c5e1560ea68125abe8c57713ab3e45e23ca4
James Stewart <jamesstewart@jamess-mbp.attlocal.net>
2018-03-29 18:30:32 -0500

    Initial commit

---
+++ Wave/ViewController.swift
@@ -0,0 +1,54 @@
+//
+//  ViewController.swift
+//  Wave
+//
+//  Created by James Stewart on 3/14/18.
+//  Copyright © 2018 James Stewart. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if (!UIImagePickerController.isSourceTypeAvailable(.camera)) {
+            let alert = UIAlertController(title: "Error", message: "This device does not have a camera.", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: NSLocalizedString("OK", comment: "Default Action."), style: .default, handler: nil))
+            self.present(alert, animated: true, completion: nil)
+            
+        }
+        
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        let chosenImage: UIImage = info[UIImagePickerControllerEditedImage] as! UIImage
+        self.imageView.image = chosenImage
+        picker.dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
+    }
+
+    @IBAction func takePhoto(_ sender: Any) {
+        let picker = UIImagePickerController.init()
+        picker.delegate = self
+        picker.allowsEditing = true
+        picker.sourceType = UIImagePickerControllerSourceType.camera
+        self.present(picker, animated: true, completion: nil)
+    }
+    
+    @IBAction func selectPhoto(_ sender: Any) {
+        let picker = UIImagePickerController.init()
+        picker.delegate = self
+        picker.allowsEditing = true
+        picker.sourceType = UIImagePickerControllerSourceType.savedPhotosAlbum
+        self.present(picker, animated: true, completion: nil)
+    }
+    
+}
+
